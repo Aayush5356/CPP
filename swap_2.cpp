f60ddfbfb63e5cacd7c826f4c233c6c93f8a33c8
Aayush Patel <136339309+Aayush5356@users.noreply.github.com>
2023-06-30 17:31:41 +0530

    Update swap_2.cpp

--- swap_2.cpp
+++ swap_2.cpp
@@ -3,7 +3,7 @@ using namespace std;
 main()
 {
 	int a,b;
-	cout << "Before Value:-" << endl;
+	cout << "Before Value:-" << endl << endl;
 	
 	cout << "Enter A :- ";
 	cin >> a ;
@@ -15,7 +15,7 @@ main()
 	b=a-b;
 	a=a-b;
 	
-	cout << "After Value:-" << endl;
+	cout << "After Value:-" << endl << endl;
 	cout << "A is :- " << a << endl;
 	cout << "B is :- " << b << endl;
 }
